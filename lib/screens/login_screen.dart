c4fa539b8efde52e212d6c9a0600c1ba42896038
AchizuePriscilla <baeewele27@gmail.com>
2021-04-23 16:29:13 +0100

    Added Spinner

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterscainitiativeproject/routes/route_names.dart';
 import 'package:flutterscainitiativeproject/shared/constants.dart';
 import 'package:flutterscainitiativeproject/shared/widgets/button.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,103 +12,112 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
-
+  bool showSpinner = false;
   String email;
   String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: RadialGradient(
-                  center: Alignment.topLeft,
-                  radius: 1,
-                  tileMode: TileMode.repeated,
-                  stops: [
-                0.3,
-                1,
-              ],
-                  colors: [
-                Colors.white,
-                Colors.lightGreenAccent.withOpacity(0.6)
-              ])),
-          padding: EdgeInsets.all(16),
-          width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      child: Image.asset("assets/images/myRecipeLogo.png"),
-                      height: MediaQuery.of(context).size.height * 0.25,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: RadialGradient(
+                    center: Alignment.topLeft,
+                    radius: 1,
+                    tileMode: TileMode.repeated,
+                    stops: [
+                  0.3,
+                  1,
+                ],
+                    colors: [
+                  Colors.white,
+                  Colors.lightGreenAccent.withOpacity(0.6)
+                ])),
+            padding: EdgeInsets.all(16),
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
                     ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.12,
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: ListView(
-                        children: [
-                          Column(
-                            children: [
-                              TextField(
-                                textAlign: TextAlign.center,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your Email"),
-                                onChanged: (value) {
-                                  email = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.02,
-                              ),
-                              TextField(
-                                textAlign: TextAlign.center,
-                                obscureText: true,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your Password"),
-                                onChanged: (value) {
-                                  password = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              Button(
-                                buttonColor: Colors.green,
-                                buttonText: 'Log in',
-                                onPressed: () async {
-                                  try {
-                                    final user =
-                                        await _auth.signInWithEmailAndPassword(
-                                            email: email, password: password);
-                                    if (user != null) {
-                                      Navigator.pushNamed(
-                                          context, RouteNames.homeScreen);
-                                    }
-                                  } catch (e) {
-                                    print(e);
-                                  }
-                                },
-                              ),
-                            ],
-                          )
-                        ],
+                    Hero(
+                      tag: 'logo',
+                      child: Container(
+                        child: Image.asset("assets/images/myRecipeLogo.png"),
+                        height: MediaQuery.of(context).size.height * 0.25,
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.12,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: ListView(
+                          children: [
+                            Column(
+                              children: [
+                                TextField(
+                                  textAlign: TextAlign.center,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your Email"),
+                                  onChanged: (value) {
+                                    email = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                ),
+                                TextField(
+                                  textAlign: TextAlign.center,
+                                  obscureText: true,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your Password"),
+                                  onChanged: (value) {
+                                    password = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                ),
+                                Button(
+                                  buttonColor: Colors.green,
+                                  buttonText: 'Log in',
+                                  onPressed: () async {
+                                    setState(() {
+                                      showSpinner = true;
+                                    });
+                                    try {
+                                      final user = await _auth
+                                          .signInWithEmailAndPassword(
+                                              email: email, password: password);
+                                      if (user != null) {
+                                        Navigator.pushNamed(
+                                            context, RouteNames.homeScreen);
+                                      }
+                                      setState(() {
+                                        showSpinner = false;
+                                      });
+                                    } catch (e) {
+                                      print(e);
+                                    }
+                                  },
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutterscainitiativeproject/routes/route_names.dart';
 import 'package:flutterscainitiativeproject/screens/home_screen.dart';
 import 'package:flutterscainitiativeproject/shared/constants.dart';
 import 'package:flutterscainitiativeproject/shared/widgets/button.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -14,6 +15,7 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
+  bool showSpinner = false;
   String firstName;
   String lastName;
   String email;
@@ -21,128 +23,137 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: RadialGradient(
-                  center: Alignment.topLeft,
-                  radius: 1,
-                  tileMode: TileMode.repeated,
-                  stops: [
-                0.3,
-                1,
-              ],
-                  colors: [
-                Colors.white,
-                Colors.lightGreenAccent.withOpacity(0.6)
-              ])),
-          padding: EdgeInsets.all(16),
-          width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      child: Image.asset("assets/images/myRecipeLogo.png"),
-                      height: MediaQuery.of(context).size.height * 0.25,
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.12,
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: ListView(
-                        children: [
-                          Column(
-                            children: [
-                              TextField(
-                                textAlign: TextAlign.center,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your First Name"),
-                                onChanged: (value) {
-                                  firstName = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.02,
-                              ),
-                              TextField(
-                                textAlign: TextAlign.center,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your Last Name"),
-                                onChanged: (value) {
-                                  lastName = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.02,
-                              ),
-                              TextField(
-                                keyboardType: TextInputType.emailAddress,
-                                textAlign: TextAlign.center,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your Email"),
-                                onChanged: (value) {
-                                  email = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.02,
-                              ),
-                              TextField(
-                                obscureText: true,
-                                textAlign: TextAlign.center,
-                                decoration: kTextFieldDecoration.copyWith(
-                                    hintText: "Enter your Password"),
-                                onChanged: (value) {
-                                  password = value;
-                                },
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              Button(
-                                buttonColor: Colors.green,
-                                buttonText: 'Register',
-                                onPressed: () async {
-                                  _firestore.collection('initials').add({
-                                    'first-name': firstName,
-                                    'last-name': lastName
-                                  });
-                                  try {
-                                    final newUser = await _auth
-                                        .createUserWithEmailAndPassword(
-                                            email: email, password: password);
-                                    if (newUser != null) {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => HomeScreen(
-                                                  firstName: firstName,
-                                                )),
-                                      );
-                                    }
-                                  } catch (e) {
-                                    print(e);
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: RadialGradient(
+                    center: Alignment.topLeft,
+                    radius: 1,
+                    tileMode: TileMode.repeated,
+                    stops: [
+                  0.3,
+                  1,
+                ],
+                    colors: [
+                  Colors.white,
+                  Colors.lightGreenAccent.withOpacity(0.6)
+                ])),
+            padding: EdgeInsets.all(16),
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: 'logo',
+                      child: Container(
+                        child: Image.asset("assets/images/myRecipeLogo.png"),
+                        height: MediaQuery.of(context).size.height * 0.25,
                       ),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.12,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: ListView(
+                          children: [
+                            Column(
+                              children: [
+                                TextField(
+                                  textAlign: TextAlign.center,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your First Name"),
+                                  onChanged: (value) {
+                                    firstName = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                ),
+                                TextField(
+                                  textAlign: TextAlign.center,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your Last Name"),
+                                  onChanged: (value) {
+                                    lastName = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                ),
+                                TextField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  textAlign: TextAlign.center,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your Email"),
+                                  onChanged: (value) {
+                                    email = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                ),
+                                TextField(
+                                  obscureText: true,
+                                  textAlign: TextAlign.center,
+                                  decoration: kTextFieldDecoration.copyWith(
+                                      hintText: "Enter your Password"),
+                                  onChanged: (value) {
+                                    password = value;
+                                  },
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                ),
+                                Button(
+                                  buttonColor: Colors.green,
+                                  buttonText: 'Register',
+                                  onPressed: () async {
+                                    setState(() {
+                                      showSpinner = true;
+                                    });
+                                    _firestore.collection('initials').add({
+                                      'first-name': firstName,
+                                      'last-name': lastName
+                                    });
+                                    try {
+                                      final newUser = await _auth
+                                          .createUserWithEmailAndPassword(
+                                              email: email, password: password);
+                                      if (newUser != null) {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => HomeScreen(
+                                                    firstName: firstName,
+                                                  )),
+                                        );
+                                      }
+                                      setState(() {
+                                        showSpinner = false;
+                                      });
+                                    } catch (e) {
+                                      print(e);
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
